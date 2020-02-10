6e39b059ec922236d30ebbbf3cbfb7e3c7e15644
Vladymyr Nazarok <VladNaz2000@gmail.com>
2020-02-10 16:29:19 +0200

    Made changes to TestModel Class. Tests passed

--- src/test/java/TestModel.java
+++ src/test/java/TestModel.java
@@ -1,12 +1,13 @@
 import org.junit.*;
 public class TestModel{
-    private Model mod;
+    private static Model mod;
     @BeforeClass
-    public void runBeforeTests(){
+    public static void runBeforeTests(){
         mod = new Model();
     }
     @Test
-    public void testGetMagicNumber(){
+    public void testGetMagicNumber()
+    {
         Assert.assertTrue(mod.getMagicNumber()>0 && mod.getMagicNumber()<=100);
     }
     @Test
